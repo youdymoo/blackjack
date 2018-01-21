f56dd97720b2c6cfd14a7f2ff2ac0973be901b1a
youdymoo <youdymoo@umich.edu>
2018-01-21 00:14:23 -0500

    Product Version

---
+++ cmake-build-debug/CMakeFiles/Project4.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/Project-4-Related-Files/card.cpp" "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/cmake-build-debug/CMakeFiles/Project4.dir/Project-4-Related-Files/card.cpp.obj"
+  "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/Project-4-Related-Files/example.cpp" "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/cmake-build-debug/CMakeFiles/Project4.dir/Project-4-Related-Files/example.cpp.obj"
+  "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/Project-4-Related-Files/rand.cpp" "C:/Users/Youdy/Documents/JI/17-Summer/Ve280/Project/Project4/cmake-build-debug/CMakeFiles/Project4.dir/Project-4-Related-Files/rand.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project4.dir/Project-4-Related-Files/card.cpp.obj"
+  "CMakeFiles/Project4.dir/Project-4-Related-Files/example.cpp.obj"
+  "CMakeFiles/Project4.dir/Project-4-Related-Files/rand.cpp.obj"
+  "Project4.pdb"
+  "Project4.exe"
+  "Project4.exe.manifest"
+  "libProject4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
